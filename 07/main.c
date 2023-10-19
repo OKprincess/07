9d8413129f26933b3ed564af0f1bb75d8b0d6de0
OKprincess <144990952+OKprincess@users.noreply.github.com>
2023-10-19 15:07:59 +0900

    Initial Commit

---
+++ 07/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  07
+//
+//  Created by ok sojoung on 2023/10/19.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
